c9d9a673ea1c4e62f93557ab546c35a0877d3c1a
Evgeniy Oshepkov <antarievg@yandex.ru>
2022-10-19 10:33:08 +0300

    fixed double code

--- game.js
+++ game.js
@@ -28,88 +28,59 @@
       computer: 0,
     };
 
+    const instructions = {
+      ru: ['игрок', 'компьютер', 'выиграл', 'ничья',
+        'ещё?', 'вы уверены?', 'результат'],
+      en: ['player', 'computer', 'win', 'draw',
+        'again?', 'are you shure?', 'result'],
+    };
+
     const lang = language === 'EN' || language === 'ENG' ?
       FIGURES_ENG : FIGURES_RUS;
 
+    const instructionsArr = language === 'EN' || language === 'ENG' ?
+      instructions.en : instructions.ru;
+
     return function start() {
-      const playerFigure = getFigure(lang);
-      const computerFigure = lang[getRandomIntInclusive()];
+      const player = getFigure(lang);
+      const computer = lang[getRandomIntInclusive()];
 
-      console.log(`${playerFigure} ${computerFigure}`);
-
-      if (language === 'EN' || language === 'ENG') {
-        if (computerFigure === 'rock' && playerFigure === 'paper' ||
-          computerFigure === 'scissors' && playerFigure === 'rock' ||
-          computerFigure === 'paper' && playerFigure === 'scissors') {
-          alert(`computer: ${computerFigure}
-            player: ${playerFigure}
-            player win`.trim());
-          result.player += 1;
-        }
-
-        if (computerFigure === 'rock' && playerFigure === 'scissors' ||
-          computerFigure === 'scissors' && playerFigure === 'paper' ||
-          computerFigure === 'paper' && playerFigure === 'rock') {
-          alert(`computer: ${computerFigure}
-            player: ${playerFigure}
-            computer win`.trim());
-          result.computer += 1;
-        }
-
-        if (computerFigure === playerFigure) {
-          alert(`computer: ${computerFigure}
-            player: ${playerFigure}
-            draw`.trim());
-        }
-
-        if (confirm('again?')) {
-          return start();
-        } else {
-          if (confirm('are you sure?')) {
-            alert(`Result:
-            Computer: ${result.computer}
-            Player: ${result.player}`.trim());
-            return;
-          } else {
-            return start();
-          }
-        }
+      if (player === computer) {
+        alert(`
+          ${instructionsArr[0]}: ${player}
+          ${instructionsArr[1]}: ${computer}
+          ${instructionsArr[3]}
+        `);
+      } else if (player === lang[0] && computer === lang[1] ||
+          player === lang[1] && computer === lang[2] ||
+          player === lang[2] && computer === lang[0]) {
+        alert(`
+              ${instructionsArr[0]}: ${player}
+              ${instructionsArr[1]}: ${computer}
+              ${instructionsArr[0]} ${instructionsArr[2]}
+            `);
+        result.player += 1;
       } else {
-        if (computerFigure === 'камень' && playerFigure === 'бумага' ||
-          computerFigure === 'ножницы' && playerFigure === 'камень' ||
-          computerFigure === 'бумага' && playerFigure === 'ножницы') {
-          alert(`компьютер: ${computerFigure}
-            игрок: ${playerFigure} 
-            игрок выиграл`.trim());
-          result.player += 1;
-        }
+        alert(`
+          ${instructionsArr[0]}: ${player}
+          ${instructionsArr[1]}: ${computer}
+          ${instructionsArr[1]} ${instructionsArr[2]}
+      `);
+        result.computer += 1;
+      }
 
-        if (computerFigure === 'камень' && playerFigure === 'ножницы' ||
-          computerFigure === 'ножницы' && playerFigure === 'бумага' ||
-          computerFigure === 'бумага' && playerFigure === 'камень') {
-          alert(`компьютер: ${computerFigure}
-            игрок: ${playerFigure}
-            компьютер выиграл`.trim());
-          result.computer += 1;
-        }
-
-        if (computerFigure === playerFigure) {
-          alert(`компьютер: ${computerFigure}
-            игрок: ${playerFigure}
-            ничья`.trim());
-        }
-
-        if (confirm('ещё?')) {
-          return start();
+      if (confirm(`${instructionsArr[4]}`)) {
+        return start();
+      } else {
+        if (confirm(`${instructionsArr[5]}`)) {
+          alert(`
+            ${instructionsArr[6]}:
+            ${instructionsArr[0]}: ${result.player}
+            ${instructionsArr[1]}: ${result.computer} 
+          `);
+          return;
         } else {
-          if (confirm('вы уверены?')) {
-            alert(`Результат:
-            Компьютер: ${result.computer}
-            Игрок: ${result.player}`.trim());
-            return;
-          } else {
-            return start();
-          }
+          return start();
         }
       }
     };
@@ -117,3 +88,4 @@
 
   window.rps = game;
 })();
+
